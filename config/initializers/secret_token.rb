0bc919123ff8f4cc18ffd688ca872864cdfe22e7
Timon <Timon.Blask@googlemail.com>
2013-12-28 21:59:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WatchMe::Application.config.secret_key_base = 'ee29b1dbb354aa193052a3318d28bfcdf4bd1d6f06600ec84db33e73ebccde49ca94f93b101bfb4538629c512b7091e4346953d5ef9a1505432e54f38b63951e'
